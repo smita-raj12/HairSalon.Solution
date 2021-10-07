58a7b51735e3c28288c136d0000eb5ff68f5dfd5
smita Raj <smita.raj12@gmail.com>
2021-10-07 10:32:33 -0700

    add display method

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -31,5 +31,11 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     } 
+   
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
   }
 }
