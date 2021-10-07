3c752cbf05cd0b2967f104f559c66fae621a75a4
smita Raj <smita.raj12@gmail.com>
2021-10-07 15:01:11 -0700

    add details method inside stylist contrller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -33,5 +33,10 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
   }
 }    
