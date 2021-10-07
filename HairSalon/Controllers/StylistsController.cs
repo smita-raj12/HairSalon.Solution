f1e7c0df89cb85ed86ab558cfac652141bc71cf2
smita Raj <smita.raj12@gmail.com>
2021-10-07 14:55:03 -0700

    add create method inside stylist controller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -20,5 +20,18 @@ namespace HairSalon.Controllers
       List<Stylist> model = _db.Stylists.ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      ViewBag.StylistId= new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }   
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }    
